ffd2e1e76f4a776dd0f87a8ba94fc88073c2ec84
rahman majeed <rahmanmajeed46@gmail.com>
2025-03-08 11:54:51 +0600

    🎉  [project] initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-orange-400 text-2xl font-bold text-center">
+        Organic Food <span className="text-black">App</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
